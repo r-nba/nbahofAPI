9ed4dce0ce618fcc3e2912bcd13039a9fde4f8c1
Sam Millner <Samuel.Millner@softwire.com>
2015-11-18 13:51:43 +0000

    Add allowance of dynamic port for heroku

--- expressmain.js
+++ expressmain.js
@@ -97,5 +97,6 @@ app.get('/', function (req, res) {
   });
 
 });
-console.log("listening on port 3000");
-app.listen(3000);
+var port = process.env.PORT || 3000;
+console.log("listening on port" + port);
+app.listen(port);
